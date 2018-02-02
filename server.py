0baf531bbba91a43d8d2abe80a7dd180cc1ab1cd
Giorgio <giorgio.architrave@gmail.com>
2018-02-02 16:51:50 +0100

    test

--- server.py
+++ server.py
@@ -16,9 +16,9 @@ def valid_miflora_mac(mac, pat=re.compile(r"C4:7C:8D:[0-9A-F]{2}:[0-9A-F]{2}:[0-
     return mac
 
 
-def poll(mac):
+def poll(mac, backend):
     """Poll data from the sensor."""
-    poller = MiFloraPoller(mac)
+    poller = MiFloraPoller(mac, backend)
     print("Getting data from Mi Flora")
     print("FW: {}".format(poller.firmware_version()))
     print("Name: {}".format(poller.name()))
@@ -36,7 +36,8 @@ def main():
     """
 
     print ('hci1')
-    poll('C4:7C:8D:65:E2:1A')
+    backend = GatttoolBackend
+    poll('C4:7C:8D:65:E2:1A', backend)
 
 
 if __name__ == '__main__':
