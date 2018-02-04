f21c1497949f28476b94bfe1a06dfd899a34883c
Giorgio <giorgio.architrave@gmail.com>
2018-02-04 13:09:13 +0100

    test

--- server.py
+++ server.py
@@ -149,6 +149,7 @@ def poll(mac, backend, ble_adapter):
 
 
 def main():
+    global miflora_plant
     """ Main function.
 
     """
@@ -157,7 +158,8 @@ def main():
     input_string_fake = "miflora_client: 180,GatttoolBackend,hci1$|$C4:7C:8D:65:E2:1A"
 
     while True:
-        print(str(socket_input_process(input_string_fake)))
+        socket_input_process(input_string_fake)
+        print(str(miflora_plant))
         sleep(1)
 
 
